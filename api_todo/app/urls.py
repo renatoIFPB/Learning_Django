563fa57d3ceb776e1ba432060c62c61a8a08fcd2
renatoIFPB <fagante08@gmail.com>
2023-03-07 12:11:14 -0300

    .

--- api_todo/app/urls.py
+++
@@ -1,7 +0,0 @@
-from app.views import TodoListAndCreate, TodoDetailChangeAndDelete
-from django.urls import path
-
-urlpatterns = [
-    path('', TodoListAndCreate.as_view()),
-    path('<int:pk>/', TodoDetailChangeAndDelete.as_view()),
-]
