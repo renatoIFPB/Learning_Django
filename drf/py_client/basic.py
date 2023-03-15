8cb15b9ee7990acb0616bbd3756191dbf1dbf9b4
renatoIFPB <fagante08@gmail.com>
2023-03-15 11:33:57 -0300

    get data

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -4,8 +4,9 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"querry": "Hello world"}) #HTTP Request
-print(get_response.text) #print raw text response
-print(get_response.status_code)
+# get_response = requests.get(endpoint, json={"querry": "Hello world"}) #HTTP Request
+get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP Request
+# print(get_response.text) #print raw text response
+# print(get_response.status_code)
 print(get_response.json())
 
